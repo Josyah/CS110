a9f242557c0550904cb929a4ca26d8539720aebc
Josyah <mjosyah@gmail.com>
2017-08-30 11:29:58 -0500

    Lab-00 Commits

---
+++ Lab-00-jmorrison17/src/Starter.java
@@ -0,0 +1,23 @@
+// YOUR NAME HERE
+// Lab-00
+// Starter.java
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Magic.println("Welcome to CS110!");
+		
+		Magic.println("Please enter an integer.");
+		int myInt1 = Magic.nextInt();
+		Magic.println("Please enter another integer.");
+		int myInt2 = Magic.nextInt();
+		
+		int mySum = myInt1 + myInt2;
+		Magic.println("The total is...");
+		Magic.println(mySum);
+
+	}
+
+}
